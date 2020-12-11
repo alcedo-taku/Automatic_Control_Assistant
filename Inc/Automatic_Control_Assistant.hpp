1ad6795a0db058a6e1c666d145a5ada1d19ce75e
立脇拓 <takuji.coroton.thanks@gmail.com>
2020-12-11 18:48:19 +0900

    TargetBasedOnTime を追加

--- Inc/Automatic_Control_Assistant.hpp
+++ Inc/Automatic_Control_Assistant.hpp
@@ -2,5 +2,6 @@
 #define AUTOMATIC_CONTROL_ASSISTANT
 
 #include "omni_wheel_controller/omni_wheel_controller.hpp"
+#include "target_based_on_time/target_based_on_time.hpp"
 
 #endif //AUTOMATIC_CONTROL_ASSISTANT
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ namespace aca
 
 ## 目次
 - [OmniWheelController](Inc/omni_wheel_controller/README.md)
+- [TargetBasedOnTime](Inc/target_based_on_time/README.md)
