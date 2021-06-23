f1bc335aa8b50010d1dba8c1325ebc96001a0c45
立脇拓 <takuji.coroton.thanks@gmail.com>
2021-06-23 16:29:43 +0900

    feat: 全体のヘッダファイルの include に PID_Controller を追加した

--- Automatic_Control_Assistant.hpp
+++ Automatic_Control_Assistant.hpp
@@ -3,5 +3,7 @@
 
 #include "omni_wheel_controller/omni_wheel_controller.hpp"
 #include "target_based_on_time/target_based_on_time.hpp"
+#include "pid_controller/PID_controller.hpp"
+#include "pid_controller/PID_controller_velocity.hpp"
 
 #endif //AUTOMATIC_CONTROL_ASSISTANT
