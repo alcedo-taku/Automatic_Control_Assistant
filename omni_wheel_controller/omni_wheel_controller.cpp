52ca1e10e7beeb589df0c32f012c320204971bd9
立脇拓 <takuji.coroton.thanks@gmail.com>
2021-08-26 11:51:00 +0900

    fix: テンプレートクラスメンバー の定義を修正した

--- omni_wheel_controller/omni_wheel_controller.cpp
+++ omni_wheel_controller/omni_wheel_controller.cpp
@@ -2,16 +2,18 @@
 
 namespace aca {
 
-OmniWheelController::OmniWheelController(
-    constexpr std::array<MechanicParameter, NUMBER_OF_OMNI_WHEELS> mechanic_parameter
+template <int NUMBER_OF_OMNI_WHEELS>
+OmniWheelController<NUMBER_OF_OMNI_WHEELS>::OmniWheelController(
+    std::array<MechanicParameter, NUMBER_OF_OMNI_WHEELS> mechanic_parameter
 ):
-    mechanicParameter(mechanic_parameter.)
+    mechanicParameter(mechanic_parameter)
 {
     std::array<MechanicParameter, NUMBER_OF_OMNI_WHEELS> mechanic_parameter;
     mechanic_parameter[0].
 }
 
-CoordinateSystem_3D OmniWheelController::rotation_matrix(CoordinateSystem_3D field_velocity, float angle){
+template <int NUMBER_OF_OMNI_WHEELS>
+OmniWheelController<NUMBER_OF_OMNI_WHEELS>::rotation_matrix(CoordinateSystem_3D field_velocity, float angle){
     CoordinateSystem_3D robot_velocity;
     robot_velocity.x =  field_velocity.x * std::cos(angle) + field_velocity.y * std::sin(angle);
     robot_velocity.y = -field_velocity.x * std::sin(angle) + field_velocity.y * std::cos(angle);
@@ -19,21 +21,26 @@ CoordinateSystem_3D OmniWheelController::rotation_matrix(CoordinateSystem_3D fie
     return robot_velocity;
 }
 
-void OmniWheelController::convert_each_wheel(CoordinateSystem_3D robot_velocity){
+
+template <int NUMBER_OF_OMNI_WHEELS>
+void OmniWheelController<NUMBER_OF_OMNI_WHEELS>::convert_each_wheel(CoordinateSystem_3D robot_velocity){
     for (const auto& e : wheel_velocity) {
         wheel_velocity = robot_velocity.x*cos(-3.0/4.0*M_PI) + robot_velocity.y*sin(-3.0/4.0*M_PI) + robot_velocity.angle*robotRadius;
     }
 }
 
-void OmniWheelController::update(CoordinateSystem_3D field_velocity, float angle){
+template <int NUMBER_OF_OMNI_WHEELS>
+void OmniWheelController<NUMBER_OF_OMNI_WHEELS>::update(CoordinateSystem_3D field_velocity, float angle){
     convert_each_wheel( rotation_matrix(field_velocity, angle) );
 }
 
-void OmniWheelController::update(float field_velocity_x, float field_velocity_y, float field_velocity_angle, float angle){
+template <int NUMBER_OF_OMNI_WHEELS>
+void OmniWheelController<NUMBER_OF_OMNI_WHEELS>::update(float field_velocity_x, float field_velocity_y, float field_velocity_angle, float angle){
     update( CoordinateSystem_3D{field_velocity_x, field_velocity_y, field_velocity_angle}, angle );
 }
 
-std::array<int16_t, NUMBER_OF_OMNI_WHEELS> OmniWheelController::get_wheel_velocity(){
+template <int NUMBER_OF_OMNI_WHEELS>
+std::array<int16_t, NUMBER_OF_OMNI_WHEELS> OmniWheelController<NUMBER_OF_OMNI_WHEELS>::get_wheel_velocity(){
     return wheel_velocity;
 }
 
