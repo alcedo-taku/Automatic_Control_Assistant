67c6c0189e3ed630f48366c411d748dff0a03632
立脇拓 <takuji.coroton.thanks@gmail.com>
2021-09-01 15:35:37 +0900

    refactor: omni_... と mecanum_... を wheel_controller の継承クラスにした

--- wheel/all.hpp
+++ wheel/all.hpp
@@ -1,7 +1,6 @@
 #ifndef AUTOMATIC_CONTROL_ASSISTANT_WHEEL_ALL_HPP
 #define AUTOMATIC_CONTROL_ASSISTANT_WHEEL_ALL_HPP
 
-#include "omni_wheel_controller.hpp"
-#include "mecanum_wheel_controller.hpp"
+#include "wheel_controller.hpp"
 
 #endif //AUTOMATIC_CONTROL_ASSISTANT_WHEEL_ALL_HPP
