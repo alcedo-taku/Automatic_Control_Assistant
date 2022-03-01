0f98d3f077410c038ff5641855993b057e5d6441
立脇拓 <takuji.coroton.thanks@gmail.com>
2022-03-01 18:07:28 +0900

    refactor: 仮引数名を変更

--- pid_controller/PID_controller.hpp
+++ pid_controller/PID_controller.hpp
@@ -23,7 +23,7 @@ public:
 	void init(const PID_Element &pid_parameter);
 	void init(const float proportional, const float integral, const float differential);
 	void init(const float frequency);
-	void updateOperation(const float current);
+	void updateOperation(const float difference);
 	void resetIntegral();
 	float getOperation();
 };
