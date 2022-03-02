f59bfc98b445487aa95b787a393b310f406fa595
立脇拓 <takuji.coroton.thanks@gmail.com>
2022-03-02 11:38:01 +0900

    feat: 許容誤差内に一定時間とどまった時、安定したとみなし、操作量を0にする機能を追加

--- pid_controller/PID_controller.cpp
+++ pid_controller/PID_controller.cpp
@@ -4,12 +4,22 @@ namespace aca {
 
 PID_controller::PID_controller(
 	const PID_Element &pid_parameter,
-	const float frequency
+	const float frequency,
+	const StableConditions &stable_conditions
 ):
-    pid_parameter(pid_parameter),
-    frequency(frequency)
+	pid_parameter(pid_parameter),
+	frequency(frequency),
+	stable_conditions(stable_conditions)
 {
 	operation.integral = 0;
+}
+
+PID_controller::PID_controller(
+	const PID_Element &pid_parameter,
+	const float frequency
+):
+	PID_controller(pid_parameter, frequency, StableConditions{0,0})
+{
 }
 
 PID_controller::PID_controller(
@@ -26,6 +36,7 @@ PID_controller::PID_controller(
 {
 }
 
+
 void PID_controller::init(const PID_Element &pid_parameter, const float frequency){
     this->pid_parameter = pid_parameter;
     this->frequency = frequency;
@@ -41,6 +52,11 @@ void PID_controller::init(const float frequency){
 }
 
 
+void PID_controller::set_stable_conditions(const float allowable_error, const uint16_t stable_time){
+	stable_conditions.allowable_error = allowable_error;
+	stable_conditions.stable_time = stable_time;
+}
+
 void PID_controller::update_operation(const float difference){
 	last_operation_value = operation_value;
 	operation.proportional = difference;
@@ -49,6 +65,19 @@ void PID_controller::update_operation(const float difference){
 	operation_value = operation.proportional * pid_parameter.proportional
 					+ operation.integral     * pid_parameter.integral
 					+ operation.differential * pid_parameter.differential;
+
+	/* 許容誤差内に一定時間とどまった時、安定したとみなす */
+	if( std::fabs(difference) < stable_conditions.allowable_error ){
+		stable_time_counter++;
+	}else{
+		stable_time_counter = 0;
+	}
+	if( stable_conditions.stable_time <= stable_time_counter && stable_conditions.stable_time != 0 ){
+		is_stable = true;
+	}else{
+		is_stable = false;
+	}
+
 	last_difference = difference;
 }
 
@@ -58,11 +87,19 @@ void PID_controller::reset_integral(){
 }
 
 float PID_controller::get_operation(){
-	return operation_value;
+	if( is_stable == true ){
+		return 0;
+	}else{
+		return operation_value;
+	}
 }
 
 float PID_controller::get_operation_difference(){
 	return operation_value - last_operation_value;
+}
+
+bool PID_controller::get_is_stable(){
+	return is_stable;
 }
 
 } // namespace aca
--- pid_controller/PID_controller.hpp
+++ pid_controller/PID_controller.hpp
@@ -1,12 +1,20 @@
 #ifndef PID_CONTROLLER_HPP
 #define PID_CONTROLLER_HPP
 
+#include <stdint.h>
+#include <cmath>
+
 namespace aca {
 
 struct PID_Element{
     float proportional;
     float integral;
     float differential;
+};
+
+struct StableConditions{
+	float allowable_error;
+	uint16_t stable_time; // 初期値(0)の場合は判定しない(常にfalse)
 };
 
 class PID_controller
@@ -18,18 +26,24 @@ private:
 	float operation_value;
 	float last_operation_value;
 	float last_difference = 0;
+	StableConditions stable_conditions;
+	uint16_t stable_time_counter;
+	bool is_stable = false;
 
 public:
+	PID_controller(const PID_Element &pid_parameter, const float frequency, const StableConditions &stable_conditions);
 	PID_controller(const PID_Element &pid_parameter, const float frequency);
 	PID_controller(const PID_Element &pid_parameter);
 	PID_controller(const float frequency);
 	void init(const PID_Element &pid_parameter, const float frequency);
 	void init(const PID_Element &pid_parameter);
 	void init(const float frequency);
+	void set_stable_conditions(const float allowable_error, const uint16_t stable_time);
 	void update_operation(const float difference);
 	void reset_integral();
 	float get_operation();
 	float get_operation_difference();
+	bool get_is_stable();
 };
 
 } // namespace aca
