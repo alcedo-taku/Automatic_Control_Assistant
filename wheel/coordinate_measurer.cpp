4f4516e60c9525a55f3dcd7a59276c216d6d55f3
@gomato63 <gomachan.6375@gmail.com>
2022-06-15 15:45:09 +0900

    feat:四角形計測追加

--- wheel/coordinate_measurer.cpp
+++ wheel/coordinate_measurer.cpp
@@ -37,6 +37,19 @@ void CoordinateMeasurer::update(const std::array<int32_t,3> &encoder_count){
 	prev_micro_field_distance = micro_field_distance;
 }
 
+void CoordinateMeasurer::update(const std::array<int32_t,4> &encoder_count){
+	std::array<float,4> distance;
+	for(uint8_t i = 0; i < 4; i++){
+		distance[i] = 2.0 * M_PI * parameter.radius_of_measure_wheel * (float)encoder_count[i] / (float)parameter.encoder_CPR;
+	}
+	coordinate.angle = calc_angle(distance) - offset_angle;
+	Coordinate<float> micro_field_distance = convert_to_field( convert_to_robot(distance) );
+	static Coordinate<float> prev_micro_field_distance = {0,0,0};
+	coordinate.x += (micro_field_distance.x + prev_micro_field_distance.x) / 2.0;
+	coordinate.y += (micro_field_distance.y + prev_micro_field_distance.y) / 2.0;
+	prev_micro_field_distance = micro_field_distance;
+}
+
 /**
  * @brief 座標を取得する関数
  * @return 座標を格納した構造体
@@ -153,6 +166,47 @@ float CoordinateMeasurerTriangle::calc_angle(std::array<float,3> distance){
 	return (distance[0] + distance[1] + distance[2]) / (3.0 * parameter.attachment_radius);
 }
 
+/*
+ * CoordinateMeasurerSquare
+ */
 
+/**
+ * @brief コンストラクタ
+ * @param encoder_PPR エンコーダの pulse per revolution
+ * @param radius_of_measure_Wheel　計測輪の半径
+ * @param attachment_radius 計測輪の取付半径
+ */
+CoordinateMeasurerSquare::CoordinateMeasurerSquare(
+	uint16_t encoder_PPR, uint16_t radius_of_measure_Wheel, uint16_t attachment_radius
+):
+	CoordinateMeasurer(encoder_PPR, radius_of_measure_Wheel, attachment_radius)
+{
+}
+
+/**
+ * @brief 各計測輪の進んだ距離を ロボット座標の微小移動距離に変換する
+ * @details 各計測輪の進んだ距離 → 各計測輪の微小進んだ距離 → ロボット座標の微小移動距離
+ * @param distance 各計測輪の進んだ距離
+ * @return ロボット座標の微小移動距離
+ */
+Coordinate<float> CoordinateMeasurerSquare::convert_to_robot(std::array<float,4> distance){
+	Coordinate<float> micro_robot_distance;
+	static std::array<float,4> prev_distance = distance;
+	micro_robot_distance.x = ((distance[0]-prev_distance[0]) - (distance[2]-prev_distance[2]))/2.0;
+	micro_robot_distance.y = ((distance[1]-prev_distance[1]) - (distance[3]-prev_distance[3]))/2.0;
+	prev_distance = distance;
+	return micro_robot_distance;
+}
+
+/**
+ * @brief 角度を計算する関数
+ * @param distance
+ * @return
+ */
+float CoordinateMeasurerSquare::calc_angle(std::array<float,4> distance){
+	return (distance[0] + distance[1] + distance[2] + distance[3])/(4.0*parameter.attachment_radius);
+}
 
 }; // namespace aca
+
+
